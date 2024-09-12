1cf926703792f0dd1de3771a501859c0ca07bd0c
Ansh Lohani <lohaniansh23@gmail.com>
2024-09-12 10:32:57 +0530

    bug with mastermind-game.py | create temp.py for testing

--- gfgs/mastermind-game/mastermind-game.py
+++ gfgs/mastermind-game/mastermind-game.py
@@ -4,34 +4,75 @@ import random
 
 def number():
     num = list()
+    temp = ""
     x = random.randint(1,9)
     num.append(x)
     for i in range(3):
         n = random.randint(0,9)
         num.append(n)
+    for i in num:
+        temp += f'{i}'
+
     return num
     
 
+def displaylist(a):
+    x = ''
+    for i in a:
+        x += f'{i}'
+    print(x)
+
+
 num = number()
 
+'''
+
+num = [1,2,3,4]
+
+'''
+
+
+
 word = ''
-wordl = list()
+gl = list()
 while True:
-    guess = input("Enter a four digit number: ")
-    if guess.isnumeric() == True:
-        if int(guess) < 9999 or int(guess) > 1000:
-            for i in range(len(guess)):
-                if guess[i] == num[i]:
-                    word += f" {guess[i]} "
-                    wordl.append(guess[i])
-                else:
-                    word += " X "
-                    wordl.append('X')
+    if word == num:
+        print("You won! Thanks for playing!")
+        ch = input("Wanna play again? (y/n): ")
+        if ch.lower() == 'n':
+            print("Thanks for playing!")
+            break 
+        else:
+            num,wnum = number()
+            word = ''
+    guess = (input("Enter a four digit number: "))
+    if guess.isalnum()==False:
+        print("Not a valid choice! Try again!")
+        continue
+    
+    else:
+        l = list()
+        t = ''
+        for i in guess:
+            l.append(int(i))
+
+        for x in range(4):
+            if num[x] == l[x]:
+                gl.insert(x,num[x])
+                t += f'{num[x]}'
+            else:
+                gl.insert(x,"X")
+                t += 'x'
+            word = t
+    print(word)
+ #      displaylist(gl)
 
                 
-       
-    else:
-        print("invalid choice! Retry")
-        pass
+        
+        
+    
+          
+        
+
 
           
